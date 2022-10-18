9f56305dfcab4544ef3b319b67cdaafc67f5e52c
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-18 13:55:28 -0700

    Add test for whileSum

--- tests/arraySum.test.js
+++ tests/arraySum.test.js
@@ -5,3 +5,7 @@ const array = [1, 2, 3, 4, 5];
 test('forSum of [1, 2, 3, 4, 5] should be 15', () => {
     expect(forSum(array)).toBe(15);
 });
+
+test('whileSum of [1, 2, 3, 4, 5] should be 15', () => {
+    expect(whileSum(array)).toBe(15);
+});
