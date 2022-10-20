515ed7acdc13d27bc7c3655f17ed45c6eb520d2f
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-19 19:09:48 -0700

    Add tests for naive and tail recursive implementations of sum

--- tests/arraySum.test.js
+++ tests/arraySum.test.js
@@ -13,3 +13,11 @@ test('whileSum of [1, 2, 3, 4, 5] should be 15', () => {
 test('reduceSum of [1, 2, 3, 4, 5] should be 15', () => {
     expect(reduceSum(array)).toBe(15);
 });
+
+test('recursiveSum of [1, 2, 3, 4, 5] should be 15', () => {
+    expect(recursiveSum(array)).toBe(15);
+});
+
+test('tailRecursiveSum of [1, 2, 3, 4, 5] should be 15', () => {
+    expect(tailRecursiveSum(array)).toBe(15);
+});
