f182348ddd9354ef8d4841194a384934cc81d8be
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-21 15:49:58 -0700

    Add insertLast method

--- LinkedList.js
+++ LinkedList.js
@@ -15,6 +15,21 @@ class LinkedList {
         this.head = new Node(data, this.head);
         this.size++;
     }
+
+    insertLast (data) {
+        // TODO: replace with a recursive implementation
+        let node = new Node(data);
+        let current;
+        if (!this.head) this.head = node;
+        else {
+            current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.size++;
+    }
 }
 
 module.exports = { LinkedList };
