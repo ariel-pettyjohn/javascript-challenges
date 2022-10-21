44e792df7e1f7cda1a46a68c2c48d770f1d23782
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-21 06:41:42 -0700

    Implement Python-style zip function

---
+++ zip.js
@@ -0,0 +1,13 @@
+function zip (...arrays) {
+    const arrayLengths   = arrays.map((array) => array.length);
+    const minArrayLength = Math.min(...arrayLengths);
+    const zippedArrays   = [];
+    for (let i = 0; i < minArrayLength; i++) {
+        const zippedArray = [];
+        arrays.forEach((array) => zippedArray.push(array[i]));
+        zippedArrays.push(zippedArray);
+    }
+    return zippedArrays;
+}
+
+module.exports = { zip };
