40c6de6c64c4fe10ce298876fa7446f23484cda4
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-21 15:51:48 -0700

    Implement removeAt method

--- LinkedList.js
+++ LinkedList.js
@@ -52,6 +52,23 @@ class LinkedList {
         this.size++;
     }
 
+    removeAt (index) {
+        if (index > 0 && index > this.size) return;
+        let current = this.head;
+        let previous;
+        let count = 0;
+        if (index === 0) this.head = current.next;
+        else {
+            while (count < index) {
+                count++;
+                previous = current;
+                current  = current.next;
+            }
+            previous.next = current.next;
+        }
+        this.size--;
+    }
+
     clearList () {
         this.head = null;
         this.size = 0;
