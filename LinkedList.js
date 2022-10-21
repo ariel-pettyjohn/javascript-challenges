950ca88d87e25764753bc2de26929c03d4c0d018
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-21 15:48:50 -0700

    Add Node and LinkedList classes along with an insertFirst method for LinkedList

---
+++ LinkedList.js
@@ -0,0 +1,20 @@
+class Node {
+    constructor (data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor () {
+        this.head = null;
+        this.size = 0;
+    }
+
+    insertFirst (data) {
+        this.head = new Node(data, this.head);
+        this.size++;
+    }
+}
+
+module.exports = { LinkedList };
