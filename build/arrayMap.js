fdeabe8e2e2639f4f7724bcd40de944731f44c96
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-22 14:56:30 -0700

    Implement arrayMap using TypeScript

--- build/arrayMap.js
+++ build/arrayMap.js
@@ -1,35 +1,28 @@
-function forMap (array, callback) {
+;
+function forMap(array, callback) {
     const _array = [...array];
     for (let i = 0; i < array.length; i++) {
         _array[i] = callback(array[i]);
     }
     return _array;
 }
-
-function whileMap (array, callback) {
-    let    index = 0;
+function whileMap(array, callback) {
+    let index = 0;
     const _array = [...array];
     while (index < array.length) {
-        _array[index] = callback(array[index]); 
+        _array[index] = callback(array[index]);
         index++;
     }
     return _array;
 }
-
-function recursiveMap (array, callback) {
+function recursiveMap(array, callback) {
     return array.length === 0
         ? []
         : [callback(array[0]), ...recursiveMap(array.slice(1), callback)];
 }
-
-function tailRecursiveMap (array, callback, result = []) {
+function tailRecursiveMap(array, callback, result = []) {
     return array.length === 0
         ? result
-        : tailRecursiveMap(
-            array.slice(1), 
-            callback, 
-            [...result, callback(array[0])]
-        );
+        : tailRecursiveMap(array.slice(1), callback, [...result, callback(array[0])]);
 }
-
 module.exports = { forMap, whileMap, recursiveMap, tailRecursiveMap };
---
+++ src/algorithms/arrayMap.ts
@@ -0,0 +1,43 @@
+interface IMapCallback {
+    (x: any): any
+};
+
+function forMap (array: any[], callback: IMapCallback): any[] {
+    const _array = [...array];
+    for (let i = 0; i < array.length; i++) {
+        _array[i] = callback(array[i]);
+    }
+    return _array;
+}
+
+function whileMap (array: any[], callback: IMapCallback): any[] {
+    let    index = 0;
+    const _array = [...array];
+    while (index < array.length) {
+        _array[index] = callback(array[index]); 
+        index++;
+    }
+    return _array;
+}
+
+function recursiveMap (array: any[], callback: IMapCallback): any[] {
+    return array.length === 0
+        ? []
+        : [callback(array[0]), ...recursiveMap(array.slice(1), callback)];
+}
+
+function tailRecursiveMap (
+    array   : any[], 
+    callback: IMapCallback, 
+    result  : any[] = []
+) {
+    return array.length === 0
+        ? result
+        : tailRecursiveMap(
+            array.slice(1), 
+            callback, 
+            [...result, callback(array[0])]
+        );
+}
+
+module.exports = { forMap, whileMap, recursiveMap, tailRecursiveMap };
