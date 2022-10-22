432abd4aa9ef1967aedc9f78e8e826db1531ff99
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-22 16:25:57 -0700

    Refactor

--- src/algorithms/factorial.ts
+++ src/algorithms/factorial.ts
@@ -1,6 +1,4 @@
-function factorial (n: number): number {
-    return n === 0 ? 1 : n * factorial(n - 1);
-}
+const factorial = (n: number): number => n === 0 ? 1 : n * factorial(n - 1);
 
 function tailRecursiveFactorial (n: number, result: number = 1): number {
     return n === 0 ? result : tailRecursiveFactorial(n - 1, n * result);
