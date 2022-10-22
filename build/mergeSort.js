a77424e5431f151b2b38c205da90482f7a39535e
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-22 15:11:10 -0700

    Implement mergeSort in TypeScript

--- build/mergeSort.js
+++ build/mergeSort.js
@@ -1,21 +1,18 @@
-function mergeSort (array, callback) {
-    function merge (left, right, callback, array = []) {
+function mergeSort(array, callback) {
+    function merge(left, right, callback, array = []) {
         if (!left.length || !right.length) {
             return [...array, ...left, ...right];
-        } else if (callback(left[0], right[0])) {
+        }
+        else if (callback(left[0], right[0])) {
             return merge(left.slice(1), right, callback, [...array, left[0]]);
         }
         return merge(left, right.slice(1), callback, [...array, right[0]]);
     }
-    if (array.length < 2) return array;
-    const half  = array.length / 2;
-    const left  = array.slice(0, half);
+    if (array.length < 2)
+        return array;
+    const half = array.length / 2;
+    const left = array.slice(0, half);
     const right = array.slice(half);
-    return merge(
-        mergeSort(left, callback), 
-        mergeSort(right, callback), 
-        callback
-    );
+    return merge(mergeSort(left, callback), mergeSort(right, callback), callback);
 }
-
 module.exports = { mergeSort };
---
+++ src/algorithms/mergeSort.ts
@@ -0,0 +1,30 @@
+interface IMergeSortCallback {
+    (x: any, y: any): boolean
+}
+
+function mergeSort (array: any[], callback: IMergeSortCallback) {
+    function merge (
+        left    : any[], 
+        right   : any[], 
+        callback: IMergeSortCallback, 
+        array   : any[] = []
+    ) {
+        if (!left.length || !right.length) {
+            return [...array, ...left, ...right];
+        } else if (callback(left[0], right[0])) {
+            return merge(left.slice(1), right, callback, [...array, left[0]]);
+        }
+        return merge(left, right.slice(1), callback, [...array, right[0]]);
+    }
+    if (array.length < 2) return array;
+    const half : number = array.length / 2;
+    const left : any[]  = array.slice(0, half);
+    const right: any[]  = array.slice(half);
+    return merge(
+        mergeSort(left, callback), 
+        mergeSort(right, callback), 
+        callback
+    );
+}
+
+module.exports = { mergeSort };
