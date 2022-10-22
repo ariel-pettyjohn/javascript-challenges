cccd8a59d2c2667f293f86755812bf726d12b160
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-21 18:24:27 -0700

    Add tests for custom implementation of Set class

---
+++ data_structures/tests/SuperSet.test.js
@@ -0,0 +1,7 @@
+const SuperSet = require('../SuperSet');
+
+test(`superSetA should equal superSetB`, () => {
+    const superSetA = new SuperSet([1, 2, 3, 3, 4]);
+    const superSetB = new SuperSet([1, 2, 2, 3, 4]);
+    expect(superSetA).toEqual(superSetB);
+});
