e7e066a6d2eeb481c81828b9b5c74e8a682c6a2a
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-21 19:05:37 -0700

    Add tests for string reverse implementation

---
+++ algorithms/tests/stringReverse.test.js
@@ -0,0 +1,17 @@
+const { reverse } = require('../stringReverse');
+
+const tests = [
+    [''       , ''       ],
+    ['foo'    , 'oof'    ],
+    ['1234'   , '4321'   ],
+    ['ak o h' , 'h o ka' ],
+    ['&$)_)&$', '$&)_)$&']
+];
+
+tests.forEach(([string, expectedString]) => {
+    const text = `of ${string} should equal ${expectedString}`;
+
+    test(`reverse ${text}`, () => {
+        expect(reverse(string)).toEqual(expectedString);
+    });
+});
