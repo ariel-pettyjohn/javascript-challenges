be727a59049662f7b324e60e43ac879f7c10c350
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-21 18:41:13 -0700

    Add tests for factorial implementations

---
+++ algorithms/tests/factorial.test.js
@@ -0,0 +1,26 @@
+const { factorial, tailRecursiveFactorial } = require('../factorial');
+
+const tests = [
+    [0,      1],
+    [1,      1],
+    [2,      2],
+    [3,      6],
+    [4,     24],
+    [5,    120],
+    [6,    720],
+    [7,   5040],
+    [8,  40320],
+    [9, 362880]
+];
+
+tests.forEach(([n, expectedValue]) => {
+    const text = `of ${n} should be ${expectedValue}`;
+
+    test(`factorial ${text}`, () => {
+        expect(factorial(n)).toEqual(expectedValue);
+    });
+
+    test(`tailRecursiveFactorial ${text}`, () => {
+        expect(tailRecursiveFactorial(n)).toEqual(expectedValue);
+    });
+});
