b6bb44ae82ab81f52a40c792a1339f14fab10172
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-22 13:58:21 -0700

    Implement zip function in TypeScript

---
+++ src/algorithms/zip.ts
@@ -0,0 +1,13 @@
+function zip (...arrays: any[][]) {
+    const arrayLengths   = arrays.map((array) => array.length);
+    const minArrayLength = Math.min(...arrayLengths);
+    const zippedArrays: any[][] = [];
+    for (let i = 0; i < minArrayLength; i++) {
+        const zippedArray: any[] = [];
+        arrays.forEach((array) => zippedArray.push(array[i]));
+        zippedArrays.push(zippedArray);
+    }
+    return zippedArrays;
+}
+
+module.exports = { zip };
