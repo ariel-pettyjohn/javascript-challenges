75636f7794dbdd9c23915644a51e7268cdffb024
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-22 14:34:19 -0700

    Implement arrayReduce in TypeScript

--- build/arrayReduce.js
+++ build/arrayReduce.js
@@ -1,13 +1,10 @@
-function recursiveReduce (array, callback, initialValue) {
+function recursiveReduce(array, callback, initialValue) {
     if (array.length === 0) {
         return initialValue;
-    } else if (array.length === 1) {
+    }
+    else if (array.length === 1) {
         return array[0];
     }
-    return recursiveReduce(
-        [callback(array[0], array[1]), ...array.slice(2)], 
-        callback
-    );
+    return recursiveReduce([callback(array[0], array[1]), ...array.slice(2)], callback, initialValue);
 }
-
 module.exports = { recursiveReduce };
---
+++ src/algorithms/arrayReduce.ts
@@ -0,0 +1,22 @@
+interface ReduceCallback {
+    (x: any, y: any): boolean
+}
+
+function recursiveReduce (
+    array       : any[], 
+    callback    : ReduceCallback, 
+    initialValue: any
+): any {
+    if (array.length === 0) {
+        return initialValue;
+    } else if (array.length === 1) {
+        return array[0];
+    }
+    return recursiveReduce(
+        [callback(array[0], array[1]), ...array.slice(2)], 
+        callback,
+        initialValue
+    );
+}
+
+module.exports = { recursiveReduce };
