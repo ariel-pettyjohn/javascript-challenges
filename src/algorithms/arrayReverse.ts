802ec51c95378b3b7d8dc0d0e6ce5a733354544c
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-22 14:25:43 -0700

    Implement arrayReverse in TypeScript

--- src/algorithms/arrayReverse.ts
+++ src/algorithms/arrayReverse.ts
@@ -1,15 +1,19 @@
-function reverse(array) {
-    if (array.length <= 1)
-        return array;
+function reverse (array: any[]): any[] {
+    if (array.length <= 1) return array;
+
     const head = array[array.length - 1];
     const tail = array.slice(0, -1);
+
     return [head, ...reverse(tail)];
 }
-function tailRecursiveReverse(array, result = []) {
-    if (array.length <= 1)
-        return array;
+
+function tailRecursiveReverse (array: any[], result: any[] = []): any[] {
+    if (array.length <=  1) return array;
+    
     const head = array[array.length - 1];
     const tail = array.slice(0, -1);
+
     return [head, ...tailRecursiveReverse(tail, [head, ...result])];
 }
+
 module.exports = { reverse, tailRecursiveReverse };
