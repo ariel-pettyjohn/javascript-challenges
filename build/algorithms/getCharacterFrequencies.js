18b691b2eeb629662fcc3a788d49b7e8a817f4af
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-23 09:28:07 -0700

    Implement getCharacterFrequencies

---
+++ build/algorithms/getCharacterFrequencies.js
@@ -0,0 +1,11 @@
+function getCharacterFrequencies(string) {
+    const _string = string.toLowerCase();
+    const characterFrequencies = {};
+    for (let i = 0; i < _string.length; i++) {
+        characterFrequencies[_string[i]]
+            ? characterFrequencies[_string[i]] += 1
+            : characterFrequencies[_string[i]] = 1;
+    }
+    return characterFrequencies;
+}
+module.exports = { getCharacterFrequencies };
---
+++ src/algorithms/getCharacterFrequencies.ts
@@ -0,0 +1,17 @@
+interface ICharacterFrequencies {
+    [index: string]: number
+}
+
+function getCharacterFrequencies (string: string): ICharacterFrequencies {
+    const _string             : string                = string.toLowerCase();
+    const characterFrequencies: ICharacterFrequencies = {};
+    for (let i: number = 0; i < _string.length; i++) {
+        characterFrequencies[_string[i]] 
+            ? characterFrequencies[_string[i]] += 1 
+            : characterFrequencies[_string[i]]  = 1;
+    }
+    return characterFrequencies;
+}
+
+module.exports = { getCharacterFrequencies };
+
