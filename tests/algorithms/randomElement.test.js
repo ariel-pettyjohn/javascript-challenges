3ab07da839ef53e2703ab45693bd740309c667fb
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-22 22:09:05 -0700

    Add test for method that selects a random element from an array

---
+++ tests/algorithms/randomElement.test.js
@@ -0,0 +1,16 @@
+const Random = require('../../build/algorithms/Random');
+
+const tests = [
+    [1, 2, 3, 4, 5],
+    [1, 'a', 2, 'b', 3, 'c'],
+    [{}, {}, {}, {}, {}, {}, {}, {}]
+];
+
+tests.forEach((array) => {
+    const text = `should return an element from ${array}`;
+
+    test(`randomElement ${text}`, () => {
+        const randomElement = Random.element(array);
+        expect(array).toContainEqual(randomElement);
+    });
+});
