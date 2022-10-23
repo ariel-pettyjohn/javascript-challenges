8f6fa62ef653d537825d89c601de82a4c9661bbb
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-23 16:11:53 -0700

    Refactor to improve legibility by directly nesting unused private static methods as functions

--- build/data-structures/SuperArray.js
+++ build/data-structures/SuperArray.js
@@ -1,93 +1,5 @@
 import { Implementation } from '../enums';
 export default class SuperArray extends Array {
-    static _forMap(array, callback) {
-        const _array = [...array];
-        for (let i = 0; i < array.length; i++) {
-            _array[i] = callback(array[i]);
-        }
-        return _array;
-    }
-    static _whileMap(array, callback) {
-        let index = 0;
-        const _array = [...array];
-        while (index < array.length) {
-            _array[index] = callback(array[index]);
-            index++;
-        }
-        return _array;
-    }
-    static _recursiveMap(array, callback) {
-        return array.length === 0
-            ? []
-            : [
-                callback(array[0]),
-                ...SuperArray._recursiveMap(array.slice(1), callback)
-            ];
-    }
-    static _tailRecursiveMap(array, callback, result = []) {
-        return array.length === 0
-            ? result
-            : SuperArray._tailRecursiveMap(array.slice(1), callback, [...result, callback(array[0])]);
-    }
-    static _tailRecursiveReduce(array, callback, initialValue) {
-        if (array.length === 0)
-            return initialValue;
-        else if (array.length === 1)
-            return array[0];
-        return SuperArray._tailRecursiveReduce([callback(array[0], array[1]), ...array.slice(2)], callback, initialValue);
-    }
-    static _recursiveReverse(array) {
-        if (array.length <= 1)
-            return array;
-        const head = array[array.length - 1];
-        const tail = array.slice(0, -1);
-        return [head, ...SuperArray._recursiveReverse(tail)];
-    }
-    static _tailRecursiveReverse(array, result = []) {
-        if (array.length <= 1)
-            return array;
-        const head = array[array.length - 1];
-        const tail = array.slice(0, -1);
-        return [
-            head,
-            ...SuperArray._tailRecursiveReverse(tail, [head, ...result])
-        ];
-    }
-    static _forSum(array) {
-        let sum = 0;
-        for (let i = 0; i < array.length; i++) {
-            sum += array[i];
-        }
-        return sum;
-    }
-    static _whileSum(array) {
-        let index = 0;
-        let sum = 0;
-        while (index < array.length) {
-            sum += array[index];
-            index++;
-        }
-        return sum;
-    }
-    static _reduceSum(array) {
-        const sum = array.reduce((x, y) => x + y, 0);
-        return sum;
-    }
-    static _superReduceSum(array) {
-        const sum = new SuperArray(...array)
-            .superReduce((x, y) => x + y, 0);
-        return sum;
-    }
-    static _recursiveSum(array) {
-        return array.length === 0
-            ? 0
-            : array[0] + SuperArray._recursiveSum(array.slice(1));
-    }
-    static _tailRecursiveSum(array, sum = 0) {
-        return array.length === 0
-            ? sum
-            : SuperArray._tailRecursiveSum(array.slice(1), array[0] + sum);
-    }
     static range(n, offset = 0) {
         if (n === 1)
             return [offset];
@@ -99,40 +11,128 @@ export default class SuperArray extends Array {
     superMap(callback, implementation = Implementation.tailRecursive) {
         switch (implementation) {
             case Implementation.for:
-                return SuperArray._forMap(this, callback);
+                function _forMap(array, callback) {
+                    const _array = [...array];
+                    for (let i = 0; i < array.length; i++) {
+                        _array[i] = callback(array[i]);
+                    }
+                    return _array;
+                }
+                return _forMap(this, callback);
             case Implementation.while:
-                return SuperArray._whileMap(this, callback);
+                function _whileMap(array, callback) {
+                    let index = 0;
+                    const _array = [...array];
+                    while (index < array.length) {
+                        _array[index] = callback(array[index]);
+                        index++;
+                    }
+                    return _array;
+                }
+                return _whileMap(this, callback);
             case Implementation.recursive:
-                return SuperArray._recursiveMap(this, callback);
+                function _recursiveMap(array, callback) {
+                    return array.length === 0
+                        ? []
+                        : [
+                            callback(array[0]),
+                            ..._recursiveMap(array.slice(1), callback)
+                        ];
+                }
+                return _recursiveMap(this, callback);
             default:
-                return SuperArray._tailRecursiveMap(this, callback);
+                function _tailRecursiveMap(array, callback, result = []) {
+                    return array.length === 0
+                        ? result
+                        : _tailRecursiveMap(array.slice(1), callback, [...result, callback(array[0])]);
+                }
+                return _tailRecursiveMap(this, callback);
         }
     }
     superReduce(callback, initialValue) {
-        return SuperArray._tailRecursiveReduce(this, callback, initialValue);
+        function _tailRecursiveReduce(array, callback, initialValue) {
+            if (array.length === 0)
+                return initialValue;
+            else if (array.length === 1)
+                return array[0];
+            return _tailRecursiveReduce([callback(array[0], array[1]), ...array.slice(2)], callback, initialValue);
+        }
+        return _tailRecursiveReduce(this, callback, initialValue);
     }
     superReverse(implementation = Implementation.tailRecursive) {
         switch (implementation) {
             case Implementation.recursive:
-                return SuperArray._recursiveReverse(this);
+                function _recursiveReverse(array) {
+                    if (array.length <= 1)
+                        return array;
+                    const head = array[array.length - 1];
+                    const tail = array.slice(0, -1);
+                    return [head, ..._recursiveReverse(tail)];
+                }
+                return _recursiveReverse(this);
             default:
-                return SuperArray._tailRecursiveReverse(this);
+                function _tailRecursiveReverse(array, result = []) {
+                    if (array.length <= 1)
+                        return array;
+                    const head = array[array.length - 1];
+                    const tail = array.slice(0, -1);
+                    return [
+                        head,
+                        ..._tailRecursiveReverse(tail, [head, ...result])
+                    ];
+                }
+                return _tailRecursiveReverse(this);
         }
     }
     superSum(implementation = Implementation.tailRecursive) {
         switch (implementation) {
             case Implementation.for:
-                return SuperArray._forSum(this);
+                function _forSum(array) {
+                    let sum = 0;
+                    for (let i = 0; i < array.length; i++) {
+                        sum += array[i];
+                    }
+                    return sum;
+                }
+                return _forSum(this);
             case Implementation.while:
-                return SuperArray._whileSum(this);
+                function _whileSum(array) {
+                    let index = 0;
+                    let sum = 0;
+                    while (index < array.length) {
+                        sum += array[index];
+                        index++;
+                    }
+                    return sum;
+                }
+                return _whileSum(this);
             case Implementation.reduce:
-                return SuperArray._reduceSum(this);
+                function _reduceSum(array) {
+                    const sum = array.reduce((x, y) => x + y, 0);
+                    return sum;
+                }
+                return _reduceSum(this);
             case Implementation.superReduce:
-                return SuperArray._superReduceSum(this);
+                function _superReduceSum(array) {
+                    const sum = new SuperArray(...array)
+                        .superReduce((x, y) => x + y, 0);
+                    return sum;
+                }
+                return _superReduceSum(this);
             case Implementation.recursive:
-                return SuperArray._recursiveSum(this);
+                function _recursiveSum(array) {
+                    return array.length === 0
+                        ? 0
+                        : array[0] + _recursiveSum(array.slice(1));
+                }
+                return _recursiveSum(this);
             default:
-                return SuperArray._tailRecursiveSum(this);
+                function _tailRecursiveSum(array, sum = 0) {
+                    return array.length === 0
+                        ? sum
+                        : _tailRecursiveSum(array.slice(1), array[0] + sum);
+                }
+                return _tailRecursiveSum(this);
         }
     }
 }
--- src/data-structures/SuperArray.ts
+++ src/data-structures/SuperArray.ts
@@ -2,126 +2,7 @@ import { IMapCallback, IReduceCallback } from '../interfaces';
 
 import { Implementation } from '../enums';
 
-export default class SuperArray extends Array<any> {
-    private static _forMap (array: any[], callback: IMapCallback): any[] {
-        const _array: any[] = [...array];
-        for (let i = 0; i < array.length; i++) {
-            _array[i] = callback(array[i]);
-        }
-        return _array;
-    }
-    
-    private static _whileMap (array: any[], callback: IMapCallback): any[] {
-        let    index: number = 0;
-        const _array: any[]  = [...array];
-        while (index < array.length) {
-            _array[index] = callback(array[index]); 
-            index++;
-        }
-        return _array;
-    }
-
-    private static _recursiveMap (array: any[], callback: IMapCallback): any[] {
-        return array.length === 0
-            ? []
-            : [
-                callback(array[0]), 
-                ...SuperArray._recursiveMap(array.slice(1), callback)
-            ];
-    }
-    
-    private static _tailRecursiveMap (
-        array   : any[], 
-        callback: IMapCallback, 
-        result  : any[] = []
-    ): any[] {
-        return array.length === 0
-            ? result
-            : SuperArray._tailRecursiveMap(
-                array.slice(1), 
-                callback, 
-                [...result, callback(array[0])]
-            );
-    }
-
-    private static _tailRecursiveReduce (
-        array       : any[], 
-        callback    : IReduceCallback, 
-        initialValue: any
-    ): any {
-        if      (array.length === 0) return initialValue;
-        else if (array.length === 1) return array[0];
-        return SuperArray._tailRecursiveReduce(
-            [callback(array[0], array[1]), ...array.slice(2)], 
-            callback,
-            initialValue
-        );
-    }
-
-    private static _recursiveReverse (array: any[]): any[] {
-        if (array.length <= 1) return array;
-        const head: any   = array[array.length - 1];
-        const tail: any[] = array.slice(0, -1);
-        return [head, ...SuperArray._recursiveReverse(tail)];
-    }
-    
-    private static _tailRecursiveReverse (
-        array : any[], 
-        result: any[] = []
-    ): any[] {
-        if (array.length <=  1) return array;
-        const head: any   = array[array.length - 1];
-        const tail: any[] = array.slice(0, -1);
-        return [
-            head, 
-            ...SuperArray._tailRecursiveReverse(tail, [head, ...result])
-        ];
-    }
-
-    private static _forSum (array: number[]): number {
-        let sum: number = 0;
-        for (let i = 0; i < array.length; i++) {
-            sum += array[i];
-        }
-        return sum;
-    }
-    
-    private static _whileSum (array: number[]): number {
-        let index: number = 0; 
-        let sum  : number = 0;
-        while (index < array.length) {
-            sum += array[index]; 
-            index++;
-        }
-        return sum;
-    }
-    
-    private static _reduceSum (array: number[]): number {
-        const sum: number = array.reduce((x, y) => x + y, 0);
-        return sum;
-    }
-    
-    private static _superReduceSum (array: number[]): number {
-        const sum: number = new SuperArray(...array)
-            .superReduce((x, y) => x + y, 0);
-        return sum;
-    }
-    
-    private static _recursiveSum (array: number[]): number {
-        return array.length === 0 
-            ? 0 
-            : array[0] + SuperArray._recursiveSum(array.slice(1));
-    }
-    
-    private static _tailRecursiveSum (
-        array: number[], 
-        sum  : number = 0
-    ): number {
-        return array.length === 0
-            ? sum
-            : SuperArray._tailRecursiveSum(array.slice(1), array[0] + sum);
-    }
-
+export default class SuperArray extends Array<any> {        
     static range (n: number, offset: number = 0): number[] {
         if (n === 1) return [offset];
         const array: number[] = [...Array(n).keys()];
@@ -136,13 +17,56 @@ export default class SuperArray extends Array<any> {
     ): any[] {
         switch (implementation) {
             case Implementation.for:
-                return SuperArray._forMap(this, callback);
+                function _forMap (array: any[], callback: IMapCallback): any[] {
+                    const _array: any[] = [...array];
+                    for (let i = 0; i < array.length; i++) {
+                        _array[i] = callback(array[i]);
+                    }
+                    return _array;
+                }
+                return _forMap(this, callback);
             case Implementation.while:
-                return SuperArray._whileMap(this, callback);
+                function _whileMap (
+                    array   : any[],
+                    callback: IMapCallback
+                ): any[] {
+                    let    index: number = 0;
+                    const _array: any[]  = [...array];
+                    while (index < array.length) {
+                        _array[index] = callback(array[index]); 
+                        index++;
+                    }
+                    return _array;
+                }
+                return _whileMap(this, callback);
             case Implementation.recursive:
-                return SuperArray._recursiveMap(this, callback);
+                function _recursiveMap (
+                    array   : any[], 
+                    callback: IMapCallback
+                ): any[] {
+                    return array.length === 0
+                        ? []
+                        : [
+                            callback(array[0]), 
+                            ..._recursiveMap(array.slice(1), callback)
+                        ];
+                }
+                return _recursiveMap(this, callback);
             default:
-                return SuperArray._tailRecursiveMap(this, callback);
+                function _tailRecursiveMap (
+                    array   : any[], 
+                    callback: IMapCallback, 
+                    result  : any[] = []
+                ): any[] {
+                    return array.length === 0
+                        ? result
+                        : _tailRecursiveMap(
+                            array.slice(1), 
+                            callback, 
+                            [...result, callback(array[0])]
+                        );
+                }
+                return _tailRecursiveMap(this, callback);
         }
     }
 
@@ -150,7 +74,20 @@ export default class SuperArray extends Array<any> {
         callback    : IReduceCallback, 
         initialValue: any
     ): any {
-        return SuperArray._tailRecursiveReduce(this, callback, initialValue);
+        function _tailRecursiveReduce (
+            array       : any[], 
+            callback    : IReduceCallback, 
+            initialValue: any
+        ): any {
+            if      (array.length === 0) return initialValue;
+            else if (array.length === 1) return array[0];
+            return _tailRecursiveReduce(
+                [callback(array[0], array[1]), ...array.slice(2)], 
+                callback,
+                initialValue
+            );
+        }
+        return _tailRecursiveReduce(this, callback, initialValue);
     }
 
     superReverse (
@@ -158,9 +95,27 @@ export default class SuperArray extends Array<any> {
     ): any {
         switch (implementation) {
             case Implementation.recursive:
-                return SuperArray._recursiveReverse(this);
+                function _recursiveReverse (array: any[]): any[] {
+                    if (array.length <= 1) return array;
+                    const head: any   = array[array.length - 1];
+                    const tail: any[] = array.slice(0, -1);
+                    return [head, ..._recursiveReverse(tail)];
+                }
+                return _recursiveReverse(this);
             default:
-                return SuperArray._tailRecursiveReverse(this);
+                function _tailRecursiveReverse (
+                    array : any[], 
+                    result: any[] = []
+                ): any[] {
+                    if (array.length <=  1) return array;
+                    const head: any   = array[array.length - 1];
+                    const tail: any[] = array.slice(0, -1);
+                    return [
+                        head, 
+                        ..._tailRecursiveReverse(tail, [head, ...result])
+                    ];
+                }
+                return _tailRecursiveReverse(this);
         }
     }
 
@@ -169,17 +124,55 @@ export default class SuperArray extends Array<any> {
     ): number {
         switch (implementation) {
             case Implementation.for:
-                return SuperArray._forSum(this);
+                function _forSum (array: number[]): number {
+                    let sum: number = 0;
+                    for (let i = 0; i < array.length; i++) {
+                        sum += array[i];
+                    }
+                    return sum;
+                }
+                return _forSum(this);
             case Implementation.while:
-                return SuperArray._whileSum(this);
+                function _whileSum (array: number[]): number {
+                    let index: number = 0; 
+                    let sum  : number = 0;
+                    while (index < array.length) {
+                        sum += array[index]; 
+                        index++;
+                    }
+                    return sum;
+                }
+                return _whileSum(this);
             case Implementation.reduce: 
-                return SuperArray._reduceSum(this); 
-            case Implementation.superReduce: 
-                return SuperArray._superReduceSum(this); 
+                function _reduceSum (array: number[]): number {
+                    const sum: number = array.reduce((x, y) => x + y, 0);
+                    return sum;
+                }
+                return _reduceSum(this); 
+            case Implementation.superReduce:
+                function _superReduceSum (array: number[]): number {
+                    const sum: number = new SuperArray(...array)
+                        .superReduce((x, y) => x + y, 0);
+                    return sum;
+                }
+                return _superReduceSum(this); 
             case Implementation.recursive:
-                return SuperArray._recursiveSum(this);
+                function _recursiveSum (array: number[]): number {
+                    return array.length === 0 
+                        ? 0 
+                        : array[0] + _recursiveSum(array.slice(1));
+                }
+                return _recursiveSum(this);
             default:
-                return SuperArray._tailRecursiveSum(this);
+                function _tailRecursiveSum (
+                    array: number[], 
+                    sum  : number = 0
+                ): number {
+                    return array.length === 0
+                        ? sum
+                        : _tailRecursiveSum(array.slice(1), array[0] + sum);
+                }
+                return _tailRecursiveSum(this);
         }
     }
 }
