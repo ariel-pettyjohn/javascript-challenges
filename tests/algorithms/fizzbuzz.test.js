a4709d663e1e51136fd58762ba552f3b89f9a30d
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-23 09:04:18 -0700

    Add test for fizzbuzz

---
+++ tests/algorithms/fizzbuzz.test.js
@@ -0,0 +1,16 @@
+const { fizzbuzz } = require('../../build/algorithms/fizzbuzz');
+
+const tests = [
+    [15, [
+        1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 
+        'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz'
+    ]]
+];
+
+tests.forEach(([input, expectedOutput]) => {
+    const text = `of ${input} should equal ${expectedOutput}`;
+
+    test(`fizzbuzz ${text}`, () => {
+        expect(fizzbuzz(input)).toEqual(expectedOutput);
+    });
+});
