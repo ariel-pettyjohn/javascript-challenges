ba79fdff5012f68260d9739800574da255925bec
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-22 22:55:53 -0700

    Add polymorphic superMap method

--- src/data-structures/SuperArray.ts
+++ src/data-structures/SuperArray.ts
@@ -2,8 +2,15 @@ interface IMapCallback {
     (x: any): any
 };
 
+enum Implementation {
+    for           = 'for',
+    while         = 'while',
+    recursive     = 'recursive',
+    tailRecursive = 'tailRecursive',
+}
+
 class SuperArray extends Array {
-    forMap (callback: IMapCallback): any[] {
+    private _forMap (callback: IMapCallback): any[] {
         const array: SuperArray = new SuperArray(...this);
         for (let i = 0; i < this.length; i++) {
             array[i] = callback(this[i]);
@@ -11,7 +18,7 @@ class SuperArray extends Array {
         return array;
     }
     
-    whileMap (callback: IMapCallback): any[] {
+    private _whileMap (callback: IMapCallback): any[] {
         let   index: number     = 0;
         const array: SuperArray = new SuperArray(...this);
         while (index < this.length) {
@@ -20,6 +27,43 @@ class SuperArray extends Array {
         }
         return array;
     } 
+
+    private _recursiveMap (callback: IMapCallback): any[] {
+        return this.length === 0
+            ? new SuperArray()
+            : new SuperArray(...[
+                callback(this[0]), 
+                ...new SuperArray(...this.slice(1))._recursiveMap(callback)
+            ]);
+    }
+
+    private _tailRecursiveMap (
+        callback: IMapCallback, 
+        result  : any[] = []
+    ): any[] {
+        return this.length === 0
+            ? result
+            : new SuperArray(...this.slice(1))._tailRecursiveMap(
+                callback, 
+                new SuperArray(...[...result, callback(this[0])])
+            );
+    }
+
+    superMap (
+        callback      : IMapCallback,
+        implementation: Implementation = Implementation.tailRecursive
+    ): any[] {
+        switch (implementation) {
+            case Implementation.for:
+                return this._forMap(callback);
+            case Implementation.while:
+                return this._whileMap(callback);
+            case Implementation.recursive:
+                return this._recursiveMap(callback);
+            default:
+                return this._tailRecursiveMap(callback);
+        }
+    }
 }
 
 module.exports = SuperArray;
