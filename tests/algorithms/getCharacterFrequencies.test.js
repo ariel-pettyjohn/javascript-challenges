90032f8682d4e7f42af26741654d30f151fb5e0c
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-23 09:27:42 -0700

    Add test for getCharacterFrequencies

---
+++ tests/algorithms/getCharacterFrequencies.test.js
@@ -0,0 +1,17 @@
+const { 
+    getCharacterFrequencies: charFreqs 
+} = require('../../build/algorithms/getCharacterFrequencies');
+
+const tests = [
+    ['foo', { f: 1, o: 2 }],
+    ['bar', { b: 1, a: 1, r: 1}],
+    ['baz', { b: 1, a: 1, z: 1}]
+];
+
+tests.forEach(([string, frequencies]) => {
+    const text = `of ${string} should equal ${frequencies}`;
+
+    test(`getCharacterFrequencies ${text}`, () => {
+        expect(charFreqs(string)).toEqual(frequencies);
+    });
+});
